21dd7ff9f0a1c36a0db1a4ad9c3436f665ec6df8
Thomas Javanaud <thomas.javanaud@precurseur.fr>
2019-12-12 21:51:49 +0100

    https://odyssey.wildcodeschool.com/quests/915?tab=challenge

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+// any CSS you require will output into a single css file (app.css in this case)
+require('../css/app.scss');
+require('../css/global.scss');
+
+const $ = require('jquery');
+
+require('bootstrap');

1ea535fd6c26c8d996dad534e2541b3d984d41c7
Thomas Javanaud <thomas.javanaud@precurseur.fr>
2019-12-17 21:38:53 +0100

    https://odyssey.wildcodeschool.com/quests/927

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,15 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("", name="index")
     */
     public function index() :Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/show/{slug?}", requirements={"slug"="([a-z0-9\-])+"}, name="show")
+    */
+    public function show($slug) :Response
+    {
+        return $this->render('wild/show.html.twig', [
+            'slug' => ucwords(str_replace("-", " ", $slug))
         ]);
     }
 }

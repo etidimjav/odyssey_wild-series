9c91ad4a2e9d58d1986ca27091ad0e1135a94785
Thomas Javanaud <thomas.javanaud@precurseur.fr>
2020-01-09 14:28:14 +0100

    https://odyssey.wildcodeschool.com/quests/946

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Program;
 use App\Repository\CategoryRepository;
+use App\Repository\EpisodeRepository;
 use App\Repository\ProgramRepository;
+use App\Repository\SeasonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -95,6 +97,74 @@ class WildController extends AbstractController
         return $this->render('wild/category.html.twig', [
             'programs' => $programs,
             'categoryName' => $categoryName
+        ]);
+    }
+
+    /**
+    * Getting seasons from one program
+    *
+    * @param string $programName The program
+    * @Route("/{programName}", name="show_program")
+    * @return Response
+    */
+    public function showByProgram(string $programName, ProgramRepository $programRepository, SeasonRepository $seasonRepository):Response
+    {
+        if (!$programName) {
+            throw $this
+                ->createNotFoundException('No programName has been sent to find season\'s.');
+        }
+        
+        $programTitle = ucwords(str_replace('-', ' ', $programName));
+        $program = $programRepository
+            ->findOneBy(['title' => mb_strtolower($programTitle)]);
+        $seasons = $seasonRepository
+            ->findBy(['program' => $program], ['id' => 'DESC']);
+
+        if (!$seasons) {
+            throw $this->createNotFoundException(
+                'No season for '.$programName.' name, found in season\'s table.'
+            );
+        }
+
+        return $this->render('wild/programs.html.twig', [
+            'seasons' => $seasons,
+            'program' => $program
+        ]);
+    }
+
+    /**
+    * Getting episodes from one program and one season
+    *
+    * @param string $programName The program
+    * @param integer $seasonNumber The season number
+    * @Route("/{programName}/{seasonNumber}", name="show_season")
+    * @return Response
+    */
+    public function showByProgramAndSeason(string $programName, int $seasonNumber, ProgramRepository $programRepository, SeasonRepository $seasonRepository, EpisodeRepository $episodeRepository):Response
+    {
+        if (!$programName || !$seasonNumber) {
+            throw $this
+                ->createNotFoundException('No programName has been sent to find season\'s.');
+        }
+        
+        $programTitle = ucwords(str_replace('-', ' ', $programName));
+        $program = $programRepository
+            ->findOneBy(['title' => mb_strtolower($programTitle)]);
+        $season = $seasonRepository
+            ->findOneBy(['number' => $seasonNumber]);
+        $episodes = $episodeRepository
+            ->findBy(['season' => $season], ['id' => 'ASC']);
+
+        if (!$episodes) {
+            throw $this->createNotFoundException(
+                'No episode for '.$programName.' name and '.$seasonNumber.' season number, found in episode\'s table.'
+            );
+        }
+
+        return $this->render('wild/seasons.html.twig', [
+            'episodes' => $episodes,
+            'programName' => $programName,
+            'seasonNumber' => $seasonNumber,
         ]);
     }
 }
